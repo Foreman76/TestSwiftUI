f1d79c5a5804bf6ca95a3f56a95c3896fa7f7237
Alexey <alexid1376@gmail.com>
2022-05-03 09:45:17 +0700

    Initial Commit

---
+++ TestSwiftUI/TestSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestSwiftUIApp.swift
+//  TestSwiftUI
+//
+//  Created by admin on 03.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct TestSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
